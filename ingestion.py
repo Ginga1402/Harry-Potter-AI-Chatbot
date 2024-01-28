64f96543a59c273fb082e2749b725b80565d914c
Ginga1402 <ishaankohli14@gmail.com>
2024-01-28 23:16:09 +0530

    first commit

---
+++ ingestion.py
@@ -0,0 +1,33 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+
+
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cuda'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+loader = DirectoryLoader('D:\Personal\Portfolio\Harry potter Novels/', glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(documents)
+
+
+
+# loader = PyPDFLoader("pet.pdf"),
+
+# documents = loader.load()
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+# texts = text_splitter.split_documents(documents)
+
+vector_store = Chroma.from_documents(texts, embeddings, collection_metadata={"hnsw:space": "cosine"}, persist_directory="stores/pet_cosine")
+
+print("Vector Store Created.......")
